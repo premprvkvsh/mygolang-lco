8a18fa3516c998f917c3794aeba1e7843876c859
Prem <pprakash312003@gmail.com>
2025-06-14 01:29:46 +0530

    initial

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "hitesh"
+	fmt.Println(username)
+	fmt.Printf("variables is of type: %T \n", username)
+
+	var smallVal bool = true
+	fmt.Println(smallVal)
+	fmt.Printf("variables is of type: %T \n", smallVal)
+}
